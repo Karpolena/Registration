018365d93dd1ea46739c221c80ec0933d57cbb16
Elena Karpenko <karp.ol3000@gmail.com>
2018-06-30 16:34:36 +0300

    Added autoprefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('scss',function(){
-    return gulp.src('./src/SCSS/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./public/css'))
+gulp.task('scss', function () {
+    return gulp.src('./src/scss/pages/**/*.scss')
+        
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./public/css'));
 });
 
-
-gulp.task('watch',function(){
-    gulp.watch('./src/SCSS/main.scss', ['scss'])
+gulp.task('scss:watch', function () {
+    gulp.watch('./src/scss/**/*.scss', ['scss']);
 });
